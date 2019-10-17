cf028501c082a954329a3e6d4c81cf2a3133cbfe
Rudolf Hersén <rudolf.hersen@schibsted.se>
2019-10-17 17:17:49 +0200

    Add function

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,31 @@
+<!--suppress HtmlUnknownAttribute -->
 <script>
-  export let name;
-</script>
-<h1>Hello {name}!</h1>
+  let promise;
 
-<style>
-  h1 {
-    color: darkslategray;
-    font-family: sans-serif;
+  async function getRandomNumber() {
+    const res = await fetch(`.netlify/functions/node-fetch/direction=n`);
+    const text = await res.text();
+
+    if (res.ok) {
+      return text;
+    } else {
+      throw new Error(text);
+    }
   }
-</style>
+
+  function handleClick() {
+    promise = getRandomNumber();
+  }
+</script>
+
+<button on:click={handleClick}>
+  generate random number
+</button>
+
+{#await promise}
+  <p>...waiting</p>
+{:then number}
+  <p>The number is {number}</p>
+{:catch error}
+  <p style="color: red">{error.message}</p>
+{/await}
